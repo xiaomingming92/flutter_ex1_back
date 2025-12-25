d249f647dc9936161d17cafdb05a0a667ac3fde0
Z2-WIN\xmm <wujixmm@gmail.com>
2025-12-25 18:32:17 +0800

    feat: 用户信息接口

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,17 @@
+/*
+ * @Author       : Z2-WIN\xmm wujixmm@gmail.com
+ * @Date         : 2025-12-11 11:45:31
+ * @LastEditors  : Z2-WIN\xmm wujixmm@gmail.com
+ * @LastEditTime : 2025-12-25 13:46:05
+ * @FilePath     : \ex1c:\Users\xmm\studioProjects\flutter_ex1_back\src\routes\index.ts
+ * @Description  : 路由模块
+ */
 import { Router } from 'express';
-import ossRoutes from './oss.js';
-import authRoutes from './auth.js';
-import articleRoutes from './article.js';
-import waterfallRoutes from './waterfall.js';
+import articleRoutes from './article';
+import authRoutes from './auth';
+import ossRoutes from './oss';
+import userRoutes from './user';
+import waterfallRoutes from './waterfall';
 
 const router = Router();
 
@@ -11,6 +20,6 @@ router.use('/oss', ossRoutes);
 router.use('/auth', authRoutes);
 router.use('/article', articleRoutes);
 router.use('/waterfall', waterfallRoutes);
+router.use('/user', userRoutes);
 
 export default router;
-
