659b7c6a33a83e80e894fb8591a67d574a590ddd
Z2-WIN\xmm <wujixmm@gmail.com>
2025-12-26 08:51:19 +0800

    chore: 修改 TypeScript 编译阶段对 ESM 模块的解析规则

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,16 +2,16 @@
  * @Author       : Z2-WIN\xmm wujixmm@gmail.com
  * @Date         : 2025-12-11 11:45:31
  * @LastEditors  : Z2-WIN\xmm wujixmm@gmail.com
- * @LastEditTime : 2025-12-25 13:46:05
+ * @LastEditTime : 2025-12-26 08:48:16
  * @FilePath     : \ex1c:\Users\xmm\studioProjects\flutter_ex1_back\src\routes\index.ts
  * @Description  : 路由模块
  */
 import { Router } from 'express';
-import articleRoutes from './article';
-import authRoutes from './auth';
-import ossRoutes from './oss';
-import userRoutes from './user';
-import waterfallRoutes from './waterfall';
+import articleRoutes from './article.js';
+import authRoutes from './auth.js';
+import ossRoutes from './oss.js';
+import userRoutes from './user.js';
+import waterfallRoutes from './waterfall.js';
 
 const router = Router();
 
